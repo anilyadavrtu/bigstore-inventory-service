e61b248b2c456e81c581e8945e5e912f32c74438
anilyadavrtu <anilyadav.rtu@gmail.com>
2020-01-16 18:00:39 +0530

    base project setup

---
+++ src/main/java/com/bigstore/bigstoreinventoryservice/BigstoreInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstoreinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigstoreInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigstoreInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigstore/bigstoreinventoryservice/BigstoreInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstoreinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigstoreInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
